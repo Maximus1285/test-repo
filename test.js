99375f69c02d2d97c84bd96e3d1b0ffd7d162da8
Marco Sanabria <msanabria@octolabs.io>
2020-01-24 14:20:27 -0600

    Add new js function

--- test.js
+++ test.js
@@ -3,5 +3,7 @@ const sum = (num1, num2) => num1 + num2;
 num1 = 2;
 num2 = 3;
 
+const substract = (num1, num2) => num1 - num2;
+
 // Testing the function
 console.log(`The result of ${num1} + ${num2} is: ${sum(num1, num2)}`);
