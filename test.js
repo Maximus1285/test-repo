2cc8696df5180185839c31855ca1b5a29d93fb3b
Marco Sanabria <marco.sanabria@gmail.com>
2020-01-24 14:31:43 -0600

    Add multiply function

--- test.js
+++ test.js
@@ -5,5 +5,7 @@ num2 = 3;
 
 const substract = (num1, num2) => num1 - num2;
 
+const multiply = (num1, num2) => num1 * num2;
+
 // Testing the function
 console.log(`The result of ${num1} + ${num2} is: ${sum(num1, num2)}`);
