eb5e45654ff481da11c53e496204aed6faf8443c
Marco Sanabria <marco.sanabria@gmail.com>
2022-04-14 09:50:09 -0600

    Update test.js in master

--- test.js
+++ test.js
@@ -7,7 +7,7 @@ const substract = (num1, num2) => num1 - num2;
 
 const multiply = (num1, num2) => num1 * num2;
 
-// Another comment
+// Another comment adding some more comments
 // Testing the function
 console.log(`The result of ${num1} + ${num2} is: ${sum(num1, num2)}`);
 
@@ -15,5 +15,6 @@ const sayHi = (name) => console.log(`Hi ${name}, how's it going?`);
 
 const formatText = (text) => {
   text = `${text.toLocaleUpperCase()} - formatted`;
+  console.log('hello world');
   return `The new string is: ${text}`;
 }
