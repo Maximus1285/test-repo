1c35961c20130da5d6a48617e21b8c8b2dcb2418
Marco Sanabria <marco.sanabria@gmail.com>
2022-04-14 11:08:00 -0600

    Update comments in test.js

--- test.js
+++ test.js
@@ -6,13 +6,14 @@ const multiply = (num1, num2) => {
   return num1 * num2;
 }
 
-// Another comment adding some more comments
-// Testing the function
+// Another comment adding some more comments edited comment here
+// Testing the function edit comment here too
 console.log(`The result of ${num1} + ${num2} is: ${sum(num1, num2)}`);
 
 const sayHi = (name) => console.log(`Hi ${name}, how's it going?`);
 
 const formatText = (text) => {
+  console.log('Another print here')
   text = `${text.toLocaleUpperCase()} - formatted`;
   console.log('hello world');
   return `The new string is: ${text}`;
